1ca43e1704443e032beab73f6efa944fe8600265
Camila Peludero <camipeludero@gmail.com>
2023-11-27 15:42:32 -0300

    Finish course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { fetchCustomersPages, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+
+  const customers = await fetchFilteredCustomers(query, currentPage);
+
+  return (
+    <>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </>
+  );
+}
